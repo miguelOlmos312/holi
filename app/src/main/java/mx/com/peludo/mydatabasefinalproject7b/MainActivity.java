096049fc78ca0c55b6f6f345a4607803d463ef66
DaliRomero <oromero1@ucol.mx>
2022-11-25 10:36:44 -0600

    Ya funciona (Creo)

--- app/src/main/java/mx/com/peludo/mydatabasefinalproject7b/MainActivity.java
+++ app/src/main/java/mx/com/peludo/mydatabasefinalproject7b/MainActivity.java
@@ -25,8 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
-    EditText buscarCampoId, buscarCampoNombre, buscarCampoDireccion;
-    ConexionSQLiteHelper con;
+
 
 
     @Override
@@ -51,14 +50,6 @@ public class MainActivity extends AppCompatActivity {
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-
-        //Agregar los datos para la parte de la busqueda
-        buscarCampoId = (EditText) findViewById(R.id.txtBuscarClave);
-        buscarCampoNombre = (EditText) findViewById(R.id.txtBuscarNombre);
-        buscarCampoDireccion = (EditText) findViewById(R.id.txtBuscarDireccion);
-
-        con = new ConexionSQLiteHelper(this,
-                "db_clientes", null, 1);
 
         // Elimina lo oscuro de los botones
         navigationView.setItemIconTintList(null);
@@ -90,66 +81,5 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 
-    /**
-     * Método para borrar clientes
-     */
-    public void borrarCliente() {
-        SQLiteDatabase db = con.getWritableDatabase();
-        //id del cliente que deseamos modificar
-        String [] parametros = {buscarCampoId.getText().toString()};
-        // Borrar físicamente
-        db.delete("clientes", "id"+"=?", parametros);
-        Toast.makeText(this, "Cliente borrado", Toast.LENGTH_SHORT).show();
-        db.close();
-        borrarCampos();
-    }
 
-    /**
-     * Método para modificar clientes
-     */
-    public void cambiarCliente() {
-        SQLiteDatabase db = con.getWritableDatabase();
-        //id del cliente que deseamos modificar
-        String [] parametros = {buscarCampoId.getText().toString()};
-        // Creamos objeto contenvalues para almacenar los campos que modificaremos: nombre, direccion
-        ContentValues valores = new ContentValues();
-        valores.put("nombre", buscarCampoNombre.getText().toString());
-        valores.put("direccion", buscarCampoDireccion.getText().toString());
-        // Escribimos físicamente en la tabla clientes
-        db.update("clientes", valores, "id"+"=?", parametros );
-        Toast.makeText(this, "Cambios Realizados", Toast.LENGTH_SHORT).show();
-        db.close();
-        borrarCampos();
-    }
-
-    /**
-     * Método para consultar clientes
-     */
-    public void consultar() {
-        SQLiteDatabase db = con.getReadableDatabase();
-
-        String [] parametros = {buscarCampoId.getText().toString()};
-        String [] campos = {"nombre", "direccion"};
-
-        try {
-            Cursor cursor = db.query("clientes", campos, "id"+"=?", parametros,null,null,null);
-            cursor.moveToFirst();
-            // Imprimir los valores recuperados de la tabla en los campos
-            buscarCampoNombre.setText(cursor.getString(0));
-            buscarCampoDireccion.setText(cursor.getString(1));
-            cursor.close();
-        } catch (Exception e){
-            Toast.makeText(this, "Cliente No Registrado", Toast.LENGTH_SHORT).show();
-            borrarCampos();
-        }
-    }
-
-    /**
-     * Método para borrar campos
-     */
-    public void borrarCampos(){
-        buscarCampoId.setText(null);
-        buscarCampoNombre.setText(null);
-        buscarCampoDireccion.setText(null);
-    }
 }
